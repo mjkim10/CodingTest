b4ff2482c7bf13d1ee85ecc2369270d77f5df9f2
김민지 <120993249+mjkim10@users.noreply.github.com>
2023-08-12 23:03:22 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.js
+++ 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.js
@@ -1,18 +1,16 @@
 function solution(X, Y) {
     let numArr = [];
-    let xObj = {};
-
-    for (let x of X) xObj[x] = (xObj[x] || 0) + 1;
+    let xObj = {}, yObj = {};
     
-    for (let y of Y) {
-        if (xObj[y]) {
-            numArr.push(y); xObj[y]--;
-        }
+    for (let x of X) xObj[x] = (xObj[x] || 0) + 1;
+    for (let y of Y) yObj[y] = (yObj[y] || 0) + 1;
+    
+    for (let i = 9; i >= 0; i--) {
+        let cnt = Math.min(xObj[i]||0, yObj[i]||0);
+        while (cnt--) numArr.push(i); 
     }
-
-    numArr.sort((a,b) => b-a);
     
     return numArr.length 
-        ? numArr[0] == 0 ? '0' : numArr.join('')
+        ? numArr[0] == '0' ? '0' : numArr.join('')
         : '-1';
 }
