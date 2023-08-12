ff074bb318e635ecc0f9fe3f9c5a5710a6f48f74
김민지 <120993249+mjkim10@users.noreply.github.com>
2023-08-12 19:28:17 +0900

    [unrated] Title: 명예의 전당 (1), Time: 0.11 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.js
+++ 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.js
@@ -1,12 +1,19 @@
 function solution(k, score) {
     let arr = [];
     
+    // return score.map(v => {
+    //     if ((arr[k - 1] || -1) < v) {
+    //         let idx = (arr.length < k ? arr.length : k) - 1;
+    //         while ((arr[idx] || -1) < v && idx > -1) idx--;
+    //         arr.splice(idx + 1, 0, v);
+    //     }
+    //     return arr[(arr.length < k ? arr.length : k) - 1];
+    // });
+    
     return score.map(v => {
-        if ((arr[k - 1] || -1) < v) {
-            let idx = (arr.length < k ? arr.length : k) - 1;
-            while ((arr[idx] || -1) < v && idx > -1) idx--;
-            arr.splice(idx + 1, 0, v);
-        }
+        arr.push(v);
+        arr.sort((a,b) => b - a);
+        if (arr.length > k) arr.pop();
         return arr[(arr.length < k ? arr.length : k) - 1];
-    });
+    })
 }
