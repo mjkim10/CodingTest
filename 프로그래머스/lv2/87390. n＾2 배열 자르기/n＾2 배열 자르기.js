0109666db7c01e255ef50cd4342047609dcd5a6e
김민지 <120993249+mjkim10@users.noreply.github.com>
2023-08-16 01:26:28 +0900

    [level 2] Title: n^2 배열 자르기, Time: 5.95 ms, Memory: 43.7 MB -BaekjoonHub

--- 프로그래머스/lv2/87390. n＾2 배열 자르기/n＾2 배열 자르기.js
+++ 프로그래머스/lv2/87390. n＾2 배열 자르기/n＾2 배열 자르기.js
@@ -2,8 +2,7 @@ function solution(n, left, right) {
     var answer = [];
     
     for (let i = left; i < right + 1; i++) {
-        let row = Math.floor(i/n), col = i%n;
-        answer.push((row > col ? row : col) + 1);
+        answer.push(Math.max(Math.floor(i/n), i%n) + 1);
     }
     
     return answer;
