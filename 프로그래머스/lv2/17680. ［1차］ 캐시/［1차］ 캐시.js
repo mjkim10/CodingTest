83fc964b64ad27a1d3c56f41af1884733c44b1be
김민지 <120993249+mjkim10@users.noreply.github.com>
2023-09-03 00:16:02 +0900

    [level 2] Title: [1차] 캐시, Time: 1.53 ms, Memory: 34.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.js
@@ -0,0 +1,22 @@
+function solution(cacheSize, cities) {
+    var answer = 0;
+    let arr = [];
+    
+    if (!cacheSize) return cities.length * 5;
+    
+    for (let i = 0; i < cities.length; i++) {
+        let idx = arr.indexOf(cities[i].toLowerCase());
+        
+        if (idx > -1) {
+            arr = [...arr.slice(0, idx), ...arr.slice(idx + 1)];
+            answer++;
+        } else answer += 5;
+        
+        if (cacheSize === 1) 
+            arr = [cities[i].toLowerCase()]
+        else 
+            arr = [...arr.slice((cacheSize - 1) * -1), cities[i].toLowerCase()];
+    }
+    
+    return answer;
+}
