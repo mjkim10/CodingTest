9b084e254f093f0b65c6a09a3323c31257492790
김민지 <120993249+mjkim10@users.noreply.github.com>
2023-09-29 19:09:18 +0900

    [level 1] Title: 예산, Time: 0.14 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12982. 예산/예산.js
@@ -0,0 +1,14 @@
+function solution(d, budget) {
+    var answer = 0;
+    let sum = 0;
+    
+    d.sort((a,b) => a - b);
+    
+    for (let i = 0; i < d.length; i++) {
+        if (sum + d[i] <= budget) {
+            sum += d[i]; answer++;
+        }
+    }
+    
+    return answer;
+}
